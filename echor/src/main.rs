15bdbe4ae77282b5d91bbff79227318c94e6650c
Josue Morales <molinajosue92@hotmail.com>
2024-11-13 22:47:42 -0500

    push up test

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Command, Arg};
+use clap::{Arg, Command};
 fn main() {
     let matches = Command::new("echor")
         .version("0.1.0")
@@ -8,18 +8,43 @@ fn main() {
             Arg::new("text")
                 .value_name("TEXT")
                 .help("Input text")
-                .required(true)
+                .required(true),
         )
         .arg(
             Arg::new("omit_newline")
                 .short('n')
                 .help("Do not print newline")
-                .num_args(0)
+                .num_args(0),
         )
         .get_matches();
-    let mut text = matches.get_many::<String>("text").unwrap().map(|s| s.as_str()).collect::<Vec<&str>>().join(" ");
+    let mut text = matches
+        .get_many::<String>("text")
+        .unwrap()
+        .map(|s| s.as_str())
+        .collect::<Vec<&str>>()
+        .join(" ");
     if !matches.contains_id("omit_newline") {
         text.push('\n')
     }
     println!("{}", text)
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_cmd::Command;
+    use predicates::prelude::*;
+
+    #[test]
+    fn dies_no_args() {
+        let mut cmd = Command::cargo_bin("echor").unwrap();
+        cmd.assert()
+            .failure()
+            .stderr(predicate::str::contains("Usage"));
+    }
+
+    #[test]
+    fn runs() {
+        let mut cmd = Command::cargo_bin("echor").unwrap();
+        cmd.arg("hello").assert().success();
+    }
 }
